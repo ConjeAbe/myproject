d477d3256a96cacc9fa94a803a6e8bd2310f308c
ConjeAbe <97683402+ConjeAbe@users.noreply.github.com>
2022-01-13 22:56:13 +0800

    Add files via upload

---
+++ completename.cpp
@@ -0,0 +1,10 @@
+// completename.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "AERO ABE ACOSTA CONJE\n";
+}
+
